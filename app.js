1b1c3a46113e007b562092719b328b45d4871bb3
almoller <moller.alan@gmail.com>
2020-04-12 23:13:06 -0600

    added functional stories

--- app.js
+++ app.js
@@ -25,12 +25,6 @@ document.getElementById("Submit_Story2").onclick = function() {
 }
 
 
-document.getElementById("autoFill_S1").onclick = function() {
-    autoFill_S1();
-}
-document.getElementById("autoFill_S2").onclick = function() {
-    autoFill_S2();
-}
 
 
 
@@ -69,6 +63,8 @@ function submit_Story1() {
     var word15 = document.getElementById("S1_word-15").value;
     var word16 = document.getElementById("S1_word-16").value;
     var word17 = document.getElementById("S1_word-17").value;
+    var word18 = document.getElementById("S1_word-18").value;
+    var word19 = document.getElementById("S1_word-19").value;
     
     document.getElementById("S1_w1").innerHTML = word1;
     document.getElementById("S1_w2").innerHTML = word2;
@@ -87,6 +83,8 @@ function submit_Story1() {
     document.getElementById("S1_w15").innerHTML = word15;
     document.getElementById("S1_w16").innerHTML = word16;
     document.getElementById("S1_w17").innerHTML = word17;
+    document.getElementById("S1_w18").innerHTML = word18;
+    document.getElementById("S1_w19").innerHTML = word19;
 
     document.getElementById("Story1-Words").style.display = "none";
     document.getElementById("Story1-Complete").style.display = "block"; 
@@ -114,6 +112,9 @@ function submit_Story2() {
     var word15 = document.getElementById("S2_word-15").value;
     var word16 = document.getElementById("S2_word-16").value;
     var word17 = document.getElementById("S2_word-17").value;
+    var word18 = document.getElementById("S2_word-18").value;
+    var word19 = document.getElementById("S2_word-19").value;
+    var word20 = document.getElementById("S2_word-20").value;
     
     document.getElementById("S2_w1").innerHTML = word1;
     document.getElementById("S2_w2").innerHTML = word2;
@@ -132,6 +133,9 @@ function submit_Story2() {
     document.getElementById("S2_w15").innerHTML = word15;
     document.getElementById("S2_w16").innerHTML = word16;
     document.getElementById("S2_w17").innerHTML = word17;
+    document.getElementById("S2_w18").innerHTML = word18;
+    document.getElementById("S2_w19").innerHTML = word19;
+    document.getElementById("S2_w20").innerHTML = word20;
 
     document.getElementById("Story1-Words").style.display = "none";
     document.getElementById("Story1-Complete").style.display = "none";   
@@ -177,44 +181,4 @@ function clearInput_S2() {
     document.getElementById("S2_word-15").value = "";
     document.getElementById("S2_word-16").value = "";
     document.getElementById("S2_word-17").value = "";
-}
-
-function autoFill_S1() {
-    var word1 = document.getElementById("S1_word-1").value = "----------";
-    var word2 = document.getElementById("S1_word-2").value = "----------";
-    var word3 = document.getElementById("S1_word-3").value = "----------";
-    var word4 = document.getElementById("S1_word-4").value = "----------";
-    var word5 = document.getElementById("S1_word-5").value = "----------";
-    var word6 = document.getElementById("S1_word-6").value = "----------";
-    var word7 = document.getElementById("S1_word-7").value = "----------";
-    var word8 = document.getElementById("S1_word-8").value = "----------";
-    var word9 = document.getElementById("S1_word-9").value = "----------";
-    var word10 = document.getElementById("S1_word-10").value = "----------";
-    var word11 = document.getElementById("S1_word-11").value = "----------";
-    var word12 = document.getElementById("S1_word-12").value = "----------";
-    var word13 = document.getElementById("S1_word-13").value = "----------";
-    var word14 = document.getElementById("S1_word-14").value = "----------";
-    var word15 = document.getElementById("S1_word-15").value = "----------";
-    var word16 = document.getElementById("S1_word-16").value = "----------";
-    var word17 = document.getElementById("S1_word-17").value = "----------";
-}
-
-function autoFill_S2() {
-    var word1 = document.getElementById("S2_word-1").value = "----------";
-    var word2 = document.getElementById("S2_word-2").value = "----------";
-    var word3 = document.getElementById("S2_word-3").value = "----------";
-    var word4 = document.getElementById("S2_word-4").value = "----------";
-    var word5 = document.getElementById("S2_word-5").value = "----------";
-    var word6 = document.getElementById("S2_word-6").value = "----------";
-    var word7 = document.getElementById("S2_word-7").value = "----------";
-    var word8 = document.getElementById("S2_word-8").value = "----------";
-    var word9 = document.getElementById("S2_word-9").value = "----------";
-    var word10 = document.getElementById("S2_word-10").value = "----------";
-    var word11 = document.getElementById("S2_word-11").value = "----------";
-    var word12 = document.getElementById("S2_word-12").value = "----------";
-    var word13 = document.getElementById("S2_word-13").value = "----------";
-    var word14 = document.getElementById("S2_word-14").value = "----------";
-    var word15 = document.getElementById("S2_word-15").value = "----------";
-    var word16 = document.getElementById("S2_word-16").value = "----------";
-    var word17 = document.getElementById("S2_word-17").value = "----------";
 }
